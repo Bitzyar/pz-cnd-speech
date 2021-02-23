cd1a0e855e458c49e1c97d8dc7ab0fab4053a578
Bitzyar <79545631+Bitzyar@users.noreply.github.com>
2021-02-23 19:58:31 -0300

    Spanish translation
    I have a lot of knowledge of english, so i decided to translate all the phrases to help you with this awesome mod.
    Extra: I don't really know how to use github, i just created my account to translate. So excuse me if this is not the aproppiate way to make a contribution.
    -Bitzyar from Argentina

--- media/lua/client/ConditionalSpeech__PhraseSet.lua
+++ media/lua/client/ConditionalSpeech__PhraseSet.lua
@@ -27,153 +27,153 @@ ConditionalSpeech.Phrases.Dead = nil
 ]]--
 
 ConditionalSpeech.Phrases.Drunk = {
-	"feeling tipsy", "feeling good", "that's the stuff", "I could go for another", "that's the good stuff", "this is great"}
+	"me siento mareado", "que bien se siente", "necesitaba esto", "no me quejo si tomo otra", "¿habra otra?", "que bueno esta esto"}
 
 ConditionalSpeech.Phrases.HasACold = {
-	"my nose is runny", "my nose is stuffed", "I have a cold for sure", "this cold is a problem",
-	"my throat is sore", "I need to do something about this cold", "do I have a fever?"
+	"tengo la nariz roja", "tengo mocos", "me estoy sintiendo mal", "este resfrio me va a dar problemas",
+	"me duele la garganta", "necesito recuperarme", "¿tengo fiebre?"
 }
 
 ConditionalSpeech.Phrases.Windchill = {
-	"it is windy out", "it is so windy", "this wind is strong", "way too windy", "it is very windy",
-	"this wind is crazy", "this wind is insane", "the winds are howling"
+	"esta ventoso afuera", "hay mucho viento", "este viento me mueve", "esta demasiado ventoso", "parece un tornado",
+	"me esta moviendo el viento", "no me sorprenderia un tornado", "el viento ruge"
 }
 
 ConditionalSpeech.Phrases.Hyperthermia = {
-	"this heat sucks", "this heat is making me thirsty", "it is so hot", "it is way too hot",
-	"this heat is kiling me", "this heat is unbearable", "I might pass out", "I can't see straight"
+	"<SWEAR> que calor", "el calor me esta dando sed", "hace tanto calor", "estoy demasiado acalorado",
+	"el calor me esta matando", "se esta volviendo insoportable el calor", "no creo lograrlo", "ya no veo nada"
 }
 
 ConditionalSpeech.Phrases.Endurance = {
-	"time out", "I need a break", "*gasp*", "I need to rest", "I can barely run", "I an barely walk",
-	"I'm going to pass out", "I can barely move", "I can barely move my arms", "I can barely move my legs"
+	"tiempo", "necesito un descanso", "*jadeo*", "necesito descansar", "apenas puedo correr", "apenas puedo caminar",
+	"voy a pasar a la otra vida", "apenas puedo moverme", "no siento los brazos", "no siento las piernas"
 }
 
 ConditionalSpeech.Phrases.Bleeding = {
-	"I'm bleeding a bit", "I'm bleeding", "I need to stop the bleeding",
-	"I'm bleeding out", "I'm bleeding to death", "I'll die at this rate"
+	"estoy sangrando un poco", "estoy sangrando", "necesito parar el sangrado",
+	"me estoy desangrando", "estoy perdiendo mi vida", "voy a morir"
 }
 
 ConditionalSpeech.Phrases.FoodEaten = {
-	"that was a meal", "I'm full", "my stomach is full", "I'm stuffed",
-	"my stomach is stuffed", "I couldn't take another bite", "I can't eat anymore"
+	"que rico", "estoy lleno", "mi estomago no da mas", "me siento muy lleno",
+	"mi estomago esta por parir", "ya no quiero comer mas", "no puedo ni tomar otro bocado"
 }
 
 ConditionalSpeech.Phrases.Injured = {
-	"I should treat this soon", "<SARCASM>, an injury", "that's going to leave a mark", "I'm going to need first-aid"
+	"deberia de tratarme pronto", "estoy herido", "eso va a dejar marca", "<SWEAR> necesito un botiquin"
 }
 
 ConditionalSpeech.Phrases.Unhappy = {
-	"I miss my family", "this is getting hard", "how long can I keep going?", "is this my life now?",
-	"what's the point of going on", "what's the point", "what kind of life is this?", "this isn't living",
-	"I should join the zombies", "why me?", "there's no future", "this world", "there's no point"
+	"extraño a mi familia", "esto se esta poniendo feo", "¿cuanto tiempo aguantare?", "¿esta es mi vida ahora?",
+	"ya no tiene sentido seguir", "no tiene sentido seguir", "¿que clase de vida es esta?", "esto no es vivir",
+	"no puede ser tan malo ser un zombie", "¿porque a mi?", "no hay futuro", "mundo cruel", "there's no point"
+	
 }
-
 ConditionalSpeech.Phrases.Wet = {
-	"getting wet here", "I'm soaked", "I'm soaking wet", "<SARCASM>, I'm soaking wet", "I'll catch a cold at this rate", "I am drenched"
+	"estoy humedo", "estoy mojado", "estoy bastante mojado", "estoy humedo y bastante", "me voy a enfermar si sigo asi", "estoy como si me hubiera bañado"
 }
 
 ConditionalSpeech.Phrases.Sick = {
-	"I feel queasy", "I'm feeling off", "I am ill", "feeling a bit sick", "I feel sick", "I feel nauseous", "do I have a fever?", "I have a fever"
+	"me siento mal", "estoy un poco fuera de juego", "estoy resfriado", "me siento enfermo", "me siento enfermo", "tengo nauseas", "¿tengo fiebre?", "tengo fiebre"
 }
 
 ConditionalSpeech.Phrases.HeavyLoad = {
-	"*grunt*", "this is heavy", "this is alot to carry", "this is a heavy load", "<SWEAR>, this is heavy", "<PAIN>. my back"
+	"*agh*", "esto esta pesado", "esto es un poco mucho", "esto es mucho para mi espalda", "<SWEAR>, estoy cargado", "<SWEAR> mi espalda"
 }
 
 ConditionalSpeech.Phrases.Zombie = {-- Dead and Zombie Moodles seemingly only have 1 level -- they may not operate well with intensity argument in generate speech
-	"I'm turning into one of them", "this is it", "this is over", "why me?", "I'm going to turn into one of them, aren't I?"
+	"voy a ser uno de ellos", "este es el final", "esto es el fin", "¿porque a mi?", "¿voy a ser uno de ellos?"
 }
 
 ConditionalSpeech.Phrases.OnDusk = {
-	"getting dark", "looks like the sun is going down", "the sun is going down"
+	"esta oscureciendo", "el sol se esconde", "el sol se esta escondiendo"
 }
 
 ConditionalSpeech.Phrases.OnDawn = {
-	"another day", "made it to another day", "the sun is coming up", "the sun is rising"
+	"nuevo dia", "sobrevivi otro dia", "esta amaneciendo", "el sol esta saliendo"
 }
 
 ConditionalSpeech.Phrases.GunJammed = {
-	"jam", "gun is jammed", "damn thing is jammed", "jammed"
+	"encasquillada", "esta encasquillada", "se encasquillo esta porqueria", "encasquillada"
 }
 
 ConditionalSpeech.Phrases.LowAmmo = {
-	"running low", "running low on ammo", "going to need to reload", "not many shots left", "ammo is low"
+	"me quedo sin balas", "<SWEAR> me estoy quedando sin balas", "necesito recargar", "me quedan pocas balas", "pocas balas"
 }
 
 ConditionalSpeech.Phrases.OutOfAmmo = {
-	"I'm out", "empty", "no ammo", "out of ammo", "there is no ammo", "gun is out of ammo", "this is empty",
-	"this thing is empty", "gun is empty", "out of bullets", "no bullets", "there is no bullets"
+	"estoy sin balas", "sin balas", "no mas balas", "<SARCASM> ya no tengo balas", "esta vacio", "no hay mas balas", "sin balas",
+	"no hay balas", "<SWEAR> este arma esta vacia", "<SWEAR> sin municion", "<SWEAR> no tengo mas municion", "no hay balas en este arma"
 }
 
 ConditionalSpeech.Phrases.Hungry = {
-	"I could use a snack", "*stomach stirs*", "I could use <FOOD>", "I have to snack on something",
-	"<FOOD> would be nice right about now", "I should go get some food", "*stomach growls*", "I better get some food",
-	"I should get something to eat", "my stomach is rumbling", "ugh, I really need something to eat", "I could go for <FOOD>",
-	"I could go for <FOOD> right about now", "*stomach growls loudly*", "this gnawing hunger is driving me nuts!",
-	"there has got to be some food somewhere", "I need food", "aaghhh. the hunger.", "where the <SWEAR> is some food!?",
-	"I'm so hungry", "uhnnng. the hunger", "I'm starving"
+	"me vendria bien un bocado", "*rugido de estomago*", "me vendria bien comer", "necesitaria comer algo",
+	"<FOOD> me vendria bien ahora mismo", "tengo que buscar comida", "*ruge el estomago*", "mejor encuentro algo de comer",
+	"necesito comida", "me ruge el estomago", "tengo mucha hambre", "quiero comer <FOOD>",
+	"quiero comer <FOOD> pero ahora", "*ruge fuerte el estomago*", "tengo tanta hambre que me pongo loco",
+	"tiene que haber comida en algun lugar", "tengo tanta hambre", "aghhh tengo hambre", "¿donde carajos puede haber comida?",
+	"estoy tan hambriento", "agghh tengo hambre", "me muero de hambre"
 }
 
 ConditionalSpeech.Phrases.Thirst = {
-	"my mouth is dry", "*clears throat*", "I could go for some water right now.", "a sip of water would be nice.",
-	"I need something to drink.", "I should get something to drink.", "my mouth feels a bit dry.", "could use a good gulp of water right now.",
-	"I should drink something.", "my mouth is so dry", "I really should get something to drink now.", "I need some water!",
-	"there has to be water somewhere", "I need water"
+	"mi boca esta seca", "*tose*", "necesito algo de tomar", "un sorbo de agua estaria bien",
+	"necesito algo de tomar", "deberia de conseguir algo de tomar", "mi boca se siente seca", "me iria bien un galon de agua",
+	"deberia de tomar algo", "tengo la boca aspera", "necesito tomar algo pronto", "necesito agua",
+	"muero de sed", "tengo sed <SWEAR>"
 }
 
 ConditionalSpeech.Phrases.Tired = {
-	"*yawn*", "I could use a nap.", "I feel sluggish.", "I should get some sleep.", "man, I could use a lie down.", "*long yawn*",
-	"I should go to sleep soon", "I should go to bed soon", "I should probably call it a day soon", "I'm starting to get bags under my eyes.",
-	"damn I'm tired.", "*extremely long yawn*", "I'm so tired", "man, I could use some sleep.", "I think I'm starting to lose it. I really need sleep.",
-	"I can barely keep my eyes open", "I feel like I'm going to pass out", "how long has it been since I last slept?", "uhnng, I'm so tired",
-	"I really should go to sleep soon.", "*your eyelids feel heavy*", "I can barely stand. I'm so tired."
+	"*bostezo*", "una siesta no vendria mal", "me siento cansado", "tengo sueño", "quiero dormir la verdad", "*gran bostezo*",
+	"deberia de ir a dormir", "necesito dormir pronto", "seria bueno tirarse a la cama", "me estoy durmiendo",
+	"estoy cansado.", "*bostezo bastante largo*", "tengo tanto sueño", "que bien me vendria dormir", "me estoy durmiendo ,necesito dormir",
+	"apenas puedo ver", "me duermo, me duermo", "cuanto llevo sin dormir", "tengo tanto sueño",
+	"debo dormir.", "mis ojos se sienten pesados", "ya no puedo mas, me desmayo de sueño"
 }
 
 ConditionalSpeech.Phrases.Bored = {
-	"I should go do something.", "I could use something to do", "*sigh*", "there has got to be something to do", "well, this is boring",
-	"man I'm bored", "so little to do, so little to see", "I'm bored as <SWEAR>>", "*long sigh*", "so bored.", "I'm bored as hell.",
-	"*deep sigh*", "I really need something to do", "there has gotta be something to do! something! anything! aghh!",
-	"sticking spoons in my eyes would be better than this", "this is boredom is driving me insane!"
+	"deberia de hacer algo", "me siento aburrido", "*quejido*", "tiene que haber algo que hacer", "bueno, me aburro",
+	"aburrido", "que lindo esta el techo", "estoy aburrido <SWEAR>", "*quejido largo*", "estoy tan aburrido.", "aburrido como planta en maceta.",
+	"*quejido fuerte*", "enserio necesito hacer algo", "debe de haber algo entretenido para hacer <SWEAR>",
+	"cualquier cosa es mejor que mirar el techo", "que aburrimiento <SWEAR>"
 }
 
 ConditionalSpeech.Phrases.Stress = {
-	"feeling stressed", "I need to relax somehow", "*deep breath*", "I need to relax", "ugh", "I could use some relaxation",
-	"*deep long breath*", "I could really use a bit of relaxation", "can't anymore", "this fucking stress", "fuck this bullshit"
+	"me siento estresado", "necesito relajarme", "*respiracion agitada*", "necesito relajarme", "ugh", "tengo que mirar tv",
+	"*respira profundo*", "necesito distraccion", "no puedo mas", "este puto stress", "estoy hervido de stress"
 }
 
 ConditionalSpeech.Phrases.Agoraphobic = {
-	"deep breaths, deep breaths", "I hate this", "should not be outside", "I should not be doing this", "what was that?", "why can't I stay inside?"
+	"respira profundo, respira profundo", "odio esto", "no quiero estar afuera", "que horrible es estar afuera", "que miedo da estar afuera", "quiero estar adentro"
 }
 
 ConditionalSpeech.Phrases.FEAR = {
-	"uh oh!", "oh!", "ohh!", "ah!", "ahh!", "holy!", "<SWEAR>", "ah <SWEAR>!", "oh <SWEAR>!", "holy!", "holy <SWEAR_adj>!",
-	--"I need to get out of here!",
-	"why!?", "ahhh!",
-	--"I have to get the <SWEAR_adj> out of here!",
-	"why!?", "please no!", "somebody help!", "somebody help me!", "oh god! ahhh!"
+	"¡uh oh!", "¡oh!", "¡ohh!", "¡ah!", "¡ahh!", "¡dios!", "<SWEAR>", "¡ah <SWEAR>!", "¡oh <SWEAR>!", "¡mierda!", "¡dios <SWEAR_adj>!",
+	--"¡necesito irme!",
+	"tengo miedo", "¡ahhh!",
+	--"necesito irme a la mierda de aqui",
+	"¡ahh!", "¡porfavor no!", "¡que alguien me ayude!", "¡dios ayuda!", "¡oh dios! ¡ahhh!"
 }
 
-ConditionalSpeech.Phrases.Hypothermia = { "it is so cold", "brrrrr", "*shivers*" }
+ConditionalSpeech.Phrases.Hypothermia = { "esta tan frio", "brrrrr", "*tiembla*" }
 
 ConditionalSpeech.Phrases.Pain = {
-	"ouch", "ow", "argh", "that hurts", "ouch", "ow", "aagghh", "that hurts", "aaaaghhh", "arrgh", "owww",
-	"that hurts like hell", "arrrgh", "the pain", "make it stop", "aagghhh", "aaaggghhh", "oh god", "aaghh"
+	"ouch", "ow", "argh", "eso dolio", "ouch", "ow", "aagghh", "eso duele", "aaaaghhh", "arrgh", "owww",
+	"eso me dolio bastante", "arrrgh", "que dolor", "aghh <SWEAR>", "aagghhh", "aaaggghhh", "oh god", "aaghh"
 }
 
-ConditionalSpeech.Phrases.Campfire = {"*sings*"}
+ConditionalSpeech.Phrases.Campfire = {"*canta*"}
 
 -- Swears are ranked by intensity
-ConditionalSpeech.Phrases.SWEAR = {"crap", "damn", "god damn", "shit", "fuck"}
-ConditionalSpeech.Phrases.SWEAR_adj = {"heck", "hell", "shit", "fuck"}
+ConditionalSpeech.Phrases.SWEAR = {"mierda", "carajos", "puta madre", "joder", "mierda"}
+ConditionalSpeech.Phrases.SWEAR_adj = {"puta", "joder", "mierda", "puta madre"}
 
 -- useful list of plosives for stammering
 ConditionalSpeech.Phrases.Plosives = {"f", "F", "p", "P", "t", "T", "k", "K", "b", "B", "d", "D", "g", "G", "s", "S", "m", "M"}
 
 ConditionalSpeech.Phrases.FOOD = {
-	"a bite to eat", "a whole pizza", "some pizza", "a slice of pizza", "a slice of cake", "something tasty",
-	"some cake", "a bucket of chicken", "some chicken", "a Spiffo burger", "a Spiffo kid's meal",
-	"a bucket of Jay's Chicken", "an order of Jay's biscuits with gravy", "eating anything", "anything to eat", "a snack"
+	"un bocadito", "una pizza completa", "una pizza", "una porcion de pizza", "un pedazo de tarta", "algo rico",
+	"una tarta", "un pollo", "unas alitas", "unas hamburguesas de Spiffo", "algo de Spiffo",
+	"unas alitas de Jay Chickens", "un hotdog con mayonesa", "cualquier cosa que se coma", "cualquier cosa comestible", "un bocado"
 }
 
-ConditionalSpeech.Phrases.SARCASM = {"just great", "awesome", "fantastic", "just what I needed"}
+ConditionalSpeech.Phrases.SARCASM = {"que bueno", "genial", "fantastico", "lo que me faltaba"}
